390b80dc75168a35168198cdf5a91383660be0b6
HeibergVA <98165773+HeibergVA@users.noreply.github.com>
2025-05-14 16:26:54 +0200

    Update P2.ino

--- Arduino_side/P2/P2.ino
+++ Arduino_side/P2/P2.ino
@@ -20,6 +20,19 @@ String inputString = "";
 const int DXL_DIR_PIN1 = 22;  // Direction control pin for RS-485
 
 
+bool measureStart = false;
+bool haverun = true;
+bool getonce = false;
+bool getonceT = false;
+int targetTime = 3000;
+float executingMovement = false;
+double startTime = 0;
+bool motionActive = true;
+float lastMillis = millis();
+
+float start[5];
+
+
 const uint8_t DXL_ID1 = 1;  // Set your Dynamixel servo ID
 const uint8_t DXL_ID2 = 2;  // Set your Dynamixel servo ID
 const uint8_t DXL_ID4 = 4;  // Set your Dynamixel servo ID
@@ -27,7 +40,7 @@ const uint8_t DXL_ID3 = 3;  // Set your Dynamixel servo ID
 const uint8_t DXL_ID5 = 5;  // Set your Dynamixel servo ID
 const uint8_t DXL_ID6 = 6;  // Set your Dynamixel servo ID
 
-uint8_t DXL_IDs[6] = {1, 2, 4, 3, 5, 6};
+uint8_t DXL_IDs[6] = { 1, 2, 4, 3, 5, 6 };
 
 
 const uint8_t DXL_ID1b = 1;  // Set your Dynamixel servo ID
@@ -105,7 +118,7 @@ void setup() {
   //dxl1.setOperatingMode(DXL_ID1b, OP_POSITION);
   //dxl1.torqueOn(DXL_ID1b);
   // Limit the maximum velocity in Position Control Mode. Use 0 for Max speed
-  
+
   dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID1, 100);
   dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID2, 100);
   dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID4, 100);
@@ -154,9 +167,9 @@ void parseMessage(String msg) {
     if (startIndex != -1 && endIndex != -1 && endIndex > startIndex) {
       String valueStr = msg.substring(startIndex + speedTag.length(), endIndex);
       float temp = valueStr.toFloat();
-      speed[i] = convertSpeed(temp);
+      //speed[i] = convertSpeed(temp);
     } else {
-      speed[i] = 0;  // Error fallback
+      //speed[i] = 0;  // Error fallback
     }
   }
 }
@@ -180,7 +193,7 @@ float convertAngle(float angle, int id) {
   return servoAngle;
 }
 
-void loop() {
+/*void loop() {
   if (stringComplete) {
     parseMessage(inputString);
     inputString = "";
@@ -208,7 +221,95 @@ void loop() {
     Serial.print(speed[4],4);
     Serial.print(speed[5],4);
   }
+}*/
+
+/*void parseMessage(String msg) {
+  for (int i = 0; i < 6; i++) {
+    String motorTag = "M" + String(i + 1);
+    String endTag = String(i + 1) + "M";
+    int startIndex = msg.indexOf(motorTag);
+    int endIndex = msg.indexOf(endTag);
+
+    if (startIndex != -1 && endIndex != -1 && endIndex > startIndex) {
+      String valueStr = msg.substring(startIndex + motorTag.length(), endIndex);
+      float temp = valueStr.toFloat();
+      theta[i] = convertAngle(temp, i + 1);
+    } else {
+      theta[i] = 0;  // Error fallback
+    }
+  }
+}*/
+
+
+void loop() {
+
+  static float currentSpeed = 0;
+  while (!getonce) {
+    start[0] = dxl.getPresentPosition(DXL_ID1);
+    start[1] = dxl.getPresentPosition(DXL_ID2);
+    start[2] = dxl.getPresentPosition(DXL_ID4);
+    start[3] = dxl.getPresentPosition(DXL_ID3);
+    start[4] = dxl.getPresentPosition(DXL_ID5);
+    start[5] = dxl.getPresentPosition(DXL_ID6);
+
+    getonce = true;
+  }
+
+  if (stringComplete) {
+    haverun = false;
+    parseMessage(inputString);
+    delay(10);
+    inputString = "";
+    motionActive = false;
+    stringComplete = false;
+  }
+  if (!motionActive) {
+    while (getonceT) {
+      startTime = millis();
+      getonceT = true;
+    }
+  }
+  for (int i = 0; i < 6; i++) {
+    speed[i] = convertSpeed(calcSpeed(start[i], theta[i], 300, startTime));
+  }
+
+  if (speed[0] > 0) {
+
+    dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID1, speed[0] + speedA);
+    dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID2, speed[1] + speedA);
+    dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID4, speed[3] + speedA);
+    dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID3, speed[2] + speedA);
+    dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID5, speed[4] + speedA);
+    dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID6, speed[5] + speedA);
+
+    dxl.setGoalPosition(DXL_ID1, theta[0], UNIT_DEGREE);
+    dxl.setGoalPosition(DXL_ID2, theta[1], UNIT_DEGREE);
+    dxl.setGoalPosition(DXL_ID3, theta[3], UNIT_DEGREE);
+    dxl.setGoalPosition(DXL_ID4, theta[2] - 90, UNIT_DEGREE);
+    dxl.setGoalPosition(DXL_ID5, theta[4], UNIT_DEGREE);
+    dxl.setGoalPosition(DXL_ID6, theta[5], UNIT_DEGREE);
+  }
 }
+
+
+float calcSpeed(float start, float thetaEnd, float targetTimeT, float startTime) {
+
+  float currentTime = (millis() - startTime) / 100;
+
+  float targetTime = targetTimeT / 100;
+
+
+  float a_0 = start;
+  float a_1 = 0;
+  float a_2 = (3 / pow(targetTime, 2)) * (thetaEnd - start);
+  float a_3 = (-2 / pow(targetTime, 3)) * (thetaEnd - start);
+
+  float speed = a_1 + 2 * a_2 * currentTime + 3 * a_3 * pow(currentTime, 2);
+
+  return speed;
+}
+
+
 
 void serialEvent() {
   while (Serial.available()) {
